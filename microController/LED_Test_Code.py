f1f0a5dcf30f5ba19496b1b9a26891c29e533eed
Tanuj Asthana <47757557+asthanatanuj@users.noreply.github.com>
2025-10-24 22:31:27 -0700

    minor formatting edit
    minor formatting edit

--- microController/LED_Test_Code.py
+++ microController/LED_Test_Code.py
@@ -1,9 +1,3 @@
-"""
-This Raspberry Pi code was developed by newbiely.com
-This Raspberry Pi code is made available for public use without any restriction
-For comprehensive instructions and wiring diagrams, please visit:
-https://newbiely.com/tutorials/raspberry-pi/raspberry-pi-led-rgb
-"""
 
 
 import RPi.GPIO as GPIO
