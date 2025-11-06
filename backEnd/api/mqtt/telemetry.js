998f34c9499d05dc2e5d8ffad028d82da8552dae
Rishabh Jinesh <rishabhjinesh@gmail.com>
2025-11-06 07:03:52 +0000

    removed security issue

--- backEnd/api/mqtt/telemetry.js
+++ backEnd/api/mqtt/telemetry.js
@@ -4,10 +4,10 @@ const server = require(process.cwd() + "/server.js")
 const path = "/robot/telemetry"
 
 async function run(client, data) {
-	console.log("got telemetry", data, "for robot", data.robotId)
+	console.log("got telemetry", data.telemetry, "for robot", data.robotId)
 
 	const serverSentEvents = server.getServerSentEvents("streamtelemetry/" + data.robotId)
-	const dataString = JSON.stringify(data)
+	const dataString = JSON.stringify(data.telemetry)
 
 	// If any server sent event connections exist for this robot, send the update
 	for (const res of serverSentEvents) {
